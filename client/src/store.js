1b8bc8d96bb61cebe228b4ea6a583e7216059ad0
DevJ5 <36604614+DevJ5@users.noreply.github.com>
2018-12-03 11:16:12 +0100

    Fixed

--- client/src/store.js
+++ client/src/store.js
@@ -1,26 +1,29 @@
-import {createStore, applyMiddleware, combineReducers, compose} from 'redux'
-import ReduxThunk from 'redux-thunk'
-import reducers from './reducers'
-import {storeJwt, socketIo} from './middleware'
-import SocketIO from './socketio'
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import reducers from './reducers';
+import { storeJwt, socketIo } from './middleware';
+import SocketIO from './socketio';
 
-const reducer = combineReducers(reducers)
+const reducer = combineReducers(reducers);
 
-const devTools = window.devToolsExtension ? window.devToolsExtension() : f => f
+const devTools = window.devToolsExtension ? window.devToolsExtension() : f => f;
 
-const socket = new SocketIO()
+const socket = new SocketIO();
 
 const enhancer = compose(
-	applyMiddleware(ReduxThunk, storeJwt, socketIo(socket)),
-	devTools
-)
+  applyMiddleware(ReduxThunk, storeJwt, socketIo(socket)),
+  devTools
+);
 
-const store = createStore(reducer, enhancer)
+const store = createStore(reducer, enhancer);
 
 // when JWT was coming from localStorage, connect via websockets
-const initialCurrentUser = store.getState().currentUser
+const initialCurrentUser = store.getState().currentUser;
 if (initialCurrentUser) {
-  socket.connect(store.dispatch, initialCurrentUser.jwt)
+  socket.connect(
+    store.dispatch,
+    initialCurrentUser.jwt
+  );
 }
 
-export default store
+export default store;
